331d3adb5fb066ba26505050ad4d4150306499f9
Abbey Sparrow <abbey@khrome.net>
2019-11-03 20:52:46 -0700

    removed some comments

--- test/scripts/bbs.js
+++ test/scripts/bbs.js
@@ -5,48 +5,6 @@ var Color = require('ascii-art-ansi/color');
 Color.is256 = true;
 Color.useDistance('euclideanDistance');
 
-/*
-art.font('Ghost Wire BBS', 'Doom', function(err, logo){
-    art.font('No place like home', 'rusted', function(err, subtext){
-        art.table({
-            verticalBar : ' ',
-            horizontalBar : ' ',
-            intersection : ' ',
-            data:[
-                {name: art.style('current users', 'red'), value: '203'},
-                {name: 'operator', value: 'vince.vega'},
-                {name: 'dial-in', value: '(917)555-4202'},
-            ]
-        }).lines(2, function(err, table){
-            art.image({
-                filepath :'Images/sunburst_red.jpg',
-                alphabet : 'ultra-wide'
-            }).lines(2, 30).overlay(logo, {
-                x: 0,
-                y: 0,
-                style: 'blue',
-            }).overlay(subtext, {
-                x: 19,
-                y: 8,
-                style: 'yellow',
-            }).overlay(table, {
-                x: -1,
-                y: -1,
-                style: 'green',
-            }, function(err, final){
-                console.log(final);
-            });
-        });
-    });
-});
-//*/
-/*art.image({
-    filepath :'Images/sunburst.jpg',
-    alphabet : 'solid'
-}, function(err, final){
-    console.log(final);
-})*/
-//*
 art.font('I N T E R N E T', 'Doom').lines(0, 6, function(err, logo){
     art.font('Serious      Business', 'rusted', function(err, subtext){
         art.table({
@@ -82,4 +40,3 @@ art.font('I N T E R N E T', 'Doom').lines(0, 6, function(err, logo){
         });
     });
 });
-//*/
